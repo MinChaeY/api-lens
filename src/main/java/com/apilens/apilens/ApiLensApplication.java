c54f1174be3d599717555240dee4abfb60419ae7
MinChaeY <127286485+MinChaeY@users.noreply.github.com>
2026-07-21 23:26:39 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/apilens/apilens/ApiLensApplication.java
@@ -0,0 +1,13 @@
+package com.apilens.apilens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apilens/apilens/ApiLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apilens.apilens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
